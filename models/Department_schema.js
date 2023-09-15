806d60ec4814f03f445aa874d46be2366d683bca
Yosef mare <yosefmare108@gmail.com>
2023-09-15 21:10:01 +0300

    add departmentWorkers property to department schema

--- models/Department_schema.js
+++ models/Department_schema.js
@@ -5,6 +5,10 @@ const departmentSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Employees',
     },
+    departmentWorkers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+    }],
 }, {versionKey: false});
 
 const DepartmentModel = mongoose.model("departments", departmentSchema)
