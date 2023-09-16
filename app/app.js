0c565e6382496aad4b2592b2714e754d43c1362b
Yosef mare <yosefmare108@gmail.com>
2023-09-16 15:15:16 +0300

    create the employees crud operations

--- app/app.js
+++ app/app.js
@@ -1,13 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const dbConnection = require('../config/database')
-const login = require('./controllers/login')
 const cores = require('cors')
+const login = require('./controllers/login')
 const Users = require('./controllers/users_operations')
+const Employees = require('./controllers/employees_operations')
 const app = express()
 
 const LoginUser = new login()
 const users = new Users()
+const employees = new Employees()
 
 app.use(cores())
 app.use(express.json())
@@ -16,6 +18,7 @@ dbConnection()
 
 app.use('/', LoginUser.router)
 app.use('/', users.router)
+app.use('/', employees.router)
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on port: ${process.env.PORT}`);
--- models/Employee_scheme.js
+++ models/Employee_scheme.js
@@ -3,10 +3,18 @@ const employeeSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     startWorkYear :Number,
-    DepartmentID: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Departments',
-    },
+    departmentsID:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'departments',
+        },
+    ],
+    shiftsID:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'shifts',
+        },
+    ]
 }, {versionKey: false});
 
 const EmployeeModel = mongoose.model("employees", employeeSchema)
